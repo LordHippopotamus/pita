b0539f65cec3408603e50d82cc2d1f073fc15981
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2023-12-25 13:25:08 +0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+import "./global.css";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
